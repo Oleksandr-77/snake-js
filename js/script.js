176411865d367b90863dd228d1cc7e2dce6767b9
Oleksandr Kokhno <kohnoan@gmail.com>
2024-03-16 15:30:42 +0200

    add html,css and js

---
+++ js/script.js
@@ -0,0 +1,78 @@
+const gameContainer = document.getElementById("game-container");
+const snakeElement = document.getElementById("snake");
+const foodElement = document.getElementById("food");
+
+let snake = [{ x: 10, y: 10 }];
+let food = getRandomPosition();
+let direction = { x: 0, y: 0 };
+let intervalTime = 100;
+let interval = setInterval(moveSnake, intervalTime);
+
+document.addEventListener("keydown", changeDirection);
+
+function moveSnake() {
+  snake.unshift({ x: snake[0].x + direction.x, y: snake[0].y + direction.y });
+
+  if (snake[0].x === food.x && snake[0].y === food.y) {
+    eatFood();
+  } else {
+    snake.pop();
+  }
+
+  render();
+}
+
+function render() {
+  while (gameContainer.firstChild) {
+    gameContainer.removeChild(gameContainer.firstChild);
+  }
+
+  snake.forEach((segment) => {
+    const snakePart = document.createElement("div");
+    snakePart.style.gridRowStart = segment.y;
+    snakePart.style.gridColumnStart = segment.x;
+    snakePart.classList.add("snake");
+    gameContainer.appendChild(snakePart);
+  });
+
+  const foodItem = document.createElement("div");
+  foodItem.style.gridRowStart = food.y;
+  foodItem.style.gridColumnStart = food.x;
+  foodItem.classList.add("food");
+  gameContainer.appendChild(foodItem);
+}
+
+function getRandomPosition() {
+  let x = Math.floor(Math.random() * 20) + 1;
+  let y = Math.floor(Math.random() * 20) + 1;
+  return { x, y };
+}
+
+function eatFood() {
+  food = getRandomPosition();
+}
+
+function changeDirection(event) {
+  switch (event.key) {
+    case "ArrowUp":
+      if (direction.y !== 1) {
+        direction = { x: 0, y: -1 };
+      }
+      break;
+    case "ArrowDown":
+      if (direction.y !== -1) {
+        direction = { x: 0, y: 1 };
+      }
+      break;
+    case "ArrowLeft":
+      if (direction.x !== 1) {
+        direction = { x: -1, y: 0 };
+      }
+      break;
+    case "ArrowRight":
+      if (direction.x !== -1) {
+        direction = { x: 1, y: 0 };
+      }
+      break;
+  }
+}
